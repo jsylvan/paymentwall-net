f0c0060e16027af382ec1bf67b77aadc76cd74e8
jeffrey sylvan <diadem@gmail.com>
2014-10-09 17:44:18 -0400

    Basic project scaffolding

---
+++ paymentwall-net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="paymentwall_net.MvcApplication" Language="C#" %>
---
+++ paymentwall-net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(paymentwall_net.Startup))]
+namespace paymentwall_net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
